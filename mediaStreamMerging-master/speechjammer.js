2f155500659227a32dbad49c8b99349f500e801e
hiroeAbe <g14901ah@gm.tsuda.ac.jp>
2017-06-29 16:36:34 +0900

    okk58

--- mediaStreamMerging-master/speechjammer.js
+++ mediaStreamMerging-master/speechjammer.js
@@ -11,7 +11,7 @@ var SpeechJammer = {
 };
 
 
-SpeechJammer.setupSJ = function(audioStream) {
+SpeechJammer.setupSJ = function() {
   // WebAudio API 関係の初期化
   console.log("sj setup");
   var audioCtx = new AudioContext();;
@@ -21,50 +21,40 @@ SpeechJammer.setupSJ = function(audioStream) {
   var drygain = audioCtx.createGain();
   var feedback = audioCtx.createGain();
   //this.output = audioCtx.createMediaStreamDestination();
-
+  this.output = audioCtx.destination();
   //var bypass = document.getElementById("bypass").checked;
   //delay.delayTime.value = parseFloat(document.getElementById("time").value);
   //feedback.gain.value = parseFloat(document.getElementById("feedback").value);
   delay.delayTime.value = 0.3;
   feedback.gain.value = 0.4;
+}
+SpeechJammer.setupFilter = function(audioStream){
   //var mix = parseFloat(document.getElementById("mix").value);
   this.mic = audioCtx.createMediaStreamSource(audioStream);
   this.mic.connect(delay);
 
   //input.connect(delay);
 
-  /*delay.connect(wetgain);
+  delay.connect(wetgain);
   delay.connect(feedback);
   feedback.connect(delay);
-  wetgain.connect(audioCtx.destination);*/
+  wetgain.connect(this.output);
 
-  function(element) {
-    this.mic.disconnect(0);
-    this.delay.disconnect(0);
-    this.wetgain.disconnect(0);
-    if(element.checked) {
-      this.mic.connect(this.delay);
-      this.delay.connect(this.wetgain);
-      this.wetgain.connect(audioCtx.destination);
-    } else {
-      this.mic.connect(audioCtx.destination);
-    }
-  }
 
   //if(bypass) mix = 0;
   //  wetgain.gain.value = mix;
   //  drygain.gain.value = 1 - mix;
 }
 
-/*SpeechJammer.toggleFilter = function(element) {
+SpeechJammer.toggleFilter = function(element) {
   this.mic.disconnect(0);
   this.delay.disconnect(0);
   this.wetgain.disconnect(0);
   if(element.checked) {
     this.mic.connect(this.delay);
     this.delay.connect(this.wetgain);
-    this.wetgain.connect(audioCtx.destination);
+    this.wetgain.connect(this.output);
   } else {
-    this.mic.connect(audioCtx.destination);
+    this.mic.connect(this.output);
   }
-}*/
+}
