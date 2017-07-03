6d55b55e3982e868dd1b310eb10c3b245d2c51eb
hiroeAbe <g14901ah@gm.tsuda.ac.jp>
2017-07-03 21:25:03 +0900

    okk64

--- mediaStreamMerging-master/delay.js
+++ mediaStreamMerging-master/delay.js
@@ -22,10 +22,10 @@ Delay.setupDelay = function(audioStream) {
   var feedback = audioCtx.createGain();
   this.output = audioCtx.createMediaStreamDestination();
 
-  var bypass = document.getElementById("bypass").checked;
-  delay.delayTime.value = parseFloat(document.getElementById("time").value);
-  feedback.gain.value = parseFloat(document.getElementById("feedback").value);
-  var mix = parseFloat(document.getElementById("mix").value);
+  //var bypass = document.getElementById("bypass").checked;
+  //delay.delayTime.value = parseFloat(document.getElementById("time").value);
+  //feedback.gain.value = parseFloat(document.getElementById("feedback").value);
+  //var mix = parseFloat(document.getElementById("mix").value);
   this.mic = audioCtx.createMediaStreamSource(audioStream);
   this.mic.connect(input);
 
@@ -37,7 +37,17 @@ Delay.setupDelay = function(audioStream) {
   wetgain.connect(this.output);
   drygain.connect(this.output);
 
-  if(bypass) mix = 0;
-    wetgain.gain.value = mix;
-    drygain.gain.value = 1 - mix;
+  //if(bypass) mix = 0;
+  //  wetgain.gain.value = mix;
+  //  drygain.gain.value = 1 - mix;
+}
+
+const Delay.setUp = () => {
+    var bypass = document.getElementById("bypass").checked;
+    delay.delayTime.value = parseFloat(document.getElementById("time").value);
+    feedback.gain.value = parseFloat(document.getElementById("feedback").value);
+    var mix = parseFloat(document.getElementById("mix").value);
+    if(bypass) mix = 0;
+      wetgain.gain.value = mix;
+      drygain.gain.value = 1 - mix;
 }
