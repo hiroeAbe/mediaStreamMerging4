4fc432af4c2269b0f17651a8ad1cb5b7976eceed
hiroeAbe <g14901ah@gm.tsuda.ac.jp>
2017-07-04 22:59:20 +0900

    okk86

--- mediaStreamMerging-master/highFrequency.js
+++ mediaStreamMerging-master/highFrequency.js
@@ -9,21 +9,22 @@ var HighFrequency = {
   FREQ_MUL: 7000,
   QUAL_MUL: 30,
 };
+var highfrequencyNode = audioCtx.createGain();
 
 HighFrequency.setup = function() {
   // WebAudio API 関係の初期化
   console.log("HighFrequency setup");
   this.output = audioCtx.createMediaStreamDestination();
-  var highfrequencyNode = audioCtx.createGain();
+  //var highfrequencyNode = audioCtx.createGain();
   //highfrequencyNode.frequency.value = 440;
 }
 
 HighFrequency.setupFilter = function(audioStream) {
   this.mic = audioCtx.createMediaStreamSource(audioStream);
   // エフェクトを掛けて(ローパス)
-  this.mic.connect(this.highfrequencyNode);
-  this.highfrequencyNode.frequency.value = this.highfrequencyNode.frequency.value * 5;
-  this.highfrequencyNode.connect(this.output);
+  this.mic.connect(highfrequencyNode);
+  highfrequencyNode.frequency.value = highfrequencyNode.frequency.value * 5;
+  highfrequencyNode.connect(this.output);
 }
 
 HighFrequency.toggleFilter = function(element) {
