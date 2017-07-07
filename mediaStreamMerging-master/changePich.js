4521db89b0b07989de4b73adf2e4a9f0719fbff4
hiroeAbe <g14901ah@gm.tsuda.ac.jp>
2017-07-07 15:18:29 +0900

    okk96

--- mediaStreamMerging-master/changePich.js
+++ mediaStreamMerging-master/changePich.js
@@ -9,28 +9,28 @@ var ChangePich = {
   FREQ_MUL: 7000,
   QUAL_MUL: 30,
 };
+var changepichNode = audioCtx.createBufferSource();
 
 ChangePich.setup = function() {
   // WebAudio API 関係の初期化
   console.log("ChangePich setup");
   this.output = audioCtx.createMediaStreamDestination();
-  var changepichNode = audioCtx.createBufferSource();
   changepichNode.playbackRate.value = 2;
 }
 
 ChangePich.setupFilter = function(audioStream) {
   this.mic = audioCtx.createMediaStreamSource(audioStream);
   // エフェクトを掛けて(ローパス)
-  this.mic.connect(this.changepichNode);
-  this.changepichNode.connect(this.output);
+  this.mic.connect(changepichNode);
+  changepichNode.connect(this.output);
 }
 
 ChangePich.toggleFilter = function(element) {
   this.mic.disconnect(0);
-  this.changepichNode.disconnect(0);
+  changepichNode.disconnect(0);
   if(element.checked) {
-    this.mic.connect(this.changepichNode);
-    this.changepichNode.connect(this.output);
+    this.mic.connect(changepichNode);
+    changepichNode.connect(this.output);
   } else {
     this.mic.connect(this.output);
   }
