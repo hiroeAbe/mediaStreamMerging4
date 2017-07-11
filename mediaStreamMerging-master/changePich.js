791b89e000e5eb8935b9921c446adfa45b381363
hiroeAbe <g14901ah@gm.tsuda.ac.jp>
2017-07-11 14:34:33 +0900

    okk119

--- mediaStreamMerging-master/changePich.js
+++ mediaStreamMerging-master/changePich.js
@@ -80,6 +80,5 @@ document.querySelector("input#max").addEventListener("change", Setup);
   this.mic = audioctx.createMediaStreamSource(audioStream);
   // エフェクトを掛けて(ローパス)
   this.mic.connect(analyser);
-  this.mic.connect(audioctx.destination);
 
 }
