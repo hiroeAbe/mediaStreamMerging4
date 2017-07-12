6b0fa84d6aad40245b9e0f86bb845a5960fc6ab3
hiroeAbe <g14901ah@gm.tsuda.ac.jp>
2017-07-12 15:23:23 +0900

    okk138

--- mediaStreamMerging-master/pichshift.js
+++ mediaStreamMerging-master/pichshift.js
@@ -16,11 +16,24 @@ var PichShift = {
 PichShift.setupFilter = function(audioStream) {
   console.log("highpass setup");
   var processor = audioCtx.createScriptProcessor(1024, 2, 2);
+
+  const Process(ev) => {
+    var buf0 = ev.outputBuffer.getChannelData(0);
+    var buf1 = ev.outputBuffer.getChannelData(1);
+    var fs = audioCtx.sampleRate;  // Sampling frequency
+
+    for(var i = 0; i < bufsize; ++i)
+        buf0[i] = buf1[i] = (Math.sin((2 * Math.PI * 440 * i) / fs) - 0.5) * 1;
+  }
+
+  processor.onaudioprocess = Process;
   this.output = audioCtx.createMediaStreamDestination();
   this.mic = audioCtx.createMediaStreamSource(audioStream);
   // エフェクトを掛けて(ローパス)
   this.mic.connect(processor);
   processor.connect(this.output);
+
+
 }
 
 /*PichShift.toggleFilter = function(element) {
